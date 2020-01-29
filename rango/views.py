28d2e441bb363cf5952bf88f3e91a7ad032e8c20
yuuto <yuuto@172.30.208.52>
2020-01-29 21:35:39 +0000

    mapping urls

--- rango/views.py
+++ rango/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!\n" +
+                        "<a href='/rango/about/'>About</a>")
 
+def about(request):
+    return HttpResponse("Rango says here is the about page.\n" +
+                        "<a href='/rango/'>Index</a>")
