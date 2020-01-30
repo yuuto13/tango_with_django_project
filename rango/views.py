d4b111cef4737184cd5c8e3066e2c215ab5ab7ce
yuuto <yuuto@172.30.208.52>
2020-01-30 01:33:32 +0000

    Revert "mapping urls"
    This reverts commit 3379ee00de399311708c9ea4a7caf962b0760554.

--- rango/views.py
+++ rango/views.py
@@ -3,13 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    # Construct a dictionary to pass variable {{boldmessage}} to the template engine.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-
-    # Return a rendered response with the template we wish to use to send to the client.
-    return render(request, 'rango/index.html', context=context_dict)
-
+    return HttpResponse("Rango says hey there partner!\n" +
+                        "<a href='/rango/about/'>About</a>")
 
 def about(request):
-    context_dict = {'myname': 'yuuto'}
-    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("Rango says here is the about page.\n" +
+                        "<a href='/rango/'>Index</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,19 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
 from django.urls import path
 from django.urls import include
-
-from django.conf import settings
-from django.conf.urls.static import static
-
 from rango import views
-
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
